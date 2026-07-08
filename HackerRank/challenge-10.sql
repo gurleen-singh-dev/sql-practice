5b1d670a8a3a7d61e009624013ba8f62b7d6ae8e
Gurleen Singh <gurleensingh342@gmail.com>
2026-07-08 21:45:50 +0530

    add hackerrank challenge 6, 7, 8, 9 and 10 solutions

---
+++ HackerRank/challenge-10.sql
@@ -0,0 +1,2 @@
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+select DISTINCT CITY from STATION WHERE CITY like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u';
